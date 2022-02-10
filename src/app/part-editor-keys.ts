57f72faaaba16843a02f00ff3b1d17b6b8623acf
Myrmex <dfusi@hotmail.com>
2022-02-10 22:58:41 +0100

    Initial commit

---
+++ src/app/part-editor-keys.ts
@@ -0,0 +1,38 @@
+import {
+  METADATA_PART_TYPEID,
+  NOTE_PART_TYPEID,
+  TOKEN_TEXT_PART_TYPEID,
+} from '@myrmidon/cadmus-part-general-ui';
+import {
+  APPARATUS_FRAGMENT_TYPEID,
+} from '@myrmidon/cadmus-part-philology-ui';
+import { PartEditorKeys } from '@myrmidon/cadmus-core';
+
+const GENERAL = 'general';
+const PHILOLOGY = 'philology';
+const TOKEN_TEXT_LAYER_PART_TYPEID = 'it.vedph.token-text-layer';
+
+/**
+ * The parts and fragments editor keys for this UI.
+ * Each property is a part type ID, mapped to a value of type PartGroupKey,
+ * having a part property with the part's editor key, and a fragments property
+ * with the mappings between fragment type IDs and their editor keys.
+ */
+export const PART_EDITOR_KEYS: PartEditorKeys = {
+  [METADATA_PART_TYPEID]: {
+    part: GENERAL,
+  },
+  [NOTE_PART_TYPEID]: {
+    part: GENERAL,
+  },
+  [TOKEN_TEXT_PART_TYPEID]: {
+    part: GENERAL,
+  },
+  // layer parts
+  [TOKEN_TEXT_LAYER_PART_TYPEID]: {
+    part: GENERAL,
+    fragments: {
+      [APPARATUS_FRAGMENT_TYPEID]: PHILOLOGY,
+    },
+  },
+};
